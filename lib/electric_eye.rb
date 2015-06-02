77df9b3008e1cea83d7807a09c6af62fdfebba7b
Michael Pope <michael@dtcorp.com.au>
2015-06-02 14:12:15 +1000

    Move my first method from the bin script to the library and write a unit test.

--- lib/electric_eye.rb
+++ lib/electric_eye.rb
@@ -2,4 +2,14 @@ require "electric_eye/version"
 
 module ElectricEye
   # Your code goes here...
+  def foo
+    return false
+  end
+
+  # Check the directory and if it doesn't exist create it.
+  def self.check_dir
+    dir = "#{ENV['HOME']}/.electric_eye"
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    dir
+  end
 end
--- spec/electic_eye_spec.rb
+++ spec/electic_eye_spec.rb
@@ -1,7 +1,13 @@
-describe "check_dir" do
-  context "when config directory exists" do
-    it "should be true" do
+require ('spec_helper.rb')
 
+describe "check_dir" do
+  context "when config directory does not exists" do
+    it "should make and return the directory" do
+      dir = "#{ENV['HOME']}/.electric_eye"
+      Dir.exist?(dir).should == false
+      ElectricEye.check_dir
+      Dir.exist?(dir).should == true
+      Dir.rmdir(dir)
     end
   end
 end
